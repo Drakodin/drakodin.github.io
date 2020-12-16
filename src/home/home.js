b69756bd7758a084cc45f8303eed250673fe735e
Tony Tian <ttian31@gatech.edu>
2020-12-16 16:44:46 -0600

    Fix particle generation

--- src/home/home.js
+++ src/home/home.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Typography, Button, Grid, makeStyles } from '@material-ui/core';
 import { isMobile } from 'react-device-detect';
-import './home.scss';
+import { ParticleGen } from './particleGen';
 
 const HomeGraph = () => {
     return (
         <Grid>
+            <ParticleGen/>
             <Grid style={{marginTop: 20}} justify="center" container alignItems="center" alignContent="center">
                 <Grid xs={12} md={6} item container justify="center" alignItems="center" spacing={3}>
-                    <img style={{height: 357, width: 240}} src="/logo-L.png" alt="full-color logo to serve as center of CSS graph"/>
+                    <img style={{height: 357, width: 240, zIndex: 1}} src="/logo-L.png" alt="full-color logo to serve as center of CSS graph"/>
                 </Grid>
             </Grid>
         </Grid>
---
+++ src/home/particleGen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './particles.scss';
+
+export const ParticleGen = () => {
+    return (
+        <div className='anim-root'>
+            <div className='bubble bubble-p1'></div>
+        </div>
+    )
+}
