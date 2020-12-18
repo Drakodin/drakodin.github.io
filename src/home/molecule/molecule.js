369d6c9e5e8ee64e773b5c09b39a2b7406eafcfa
Tony Tian <ttian31@gatech.edu>
2020-12-17 23:31:47 -0600

    Fix particle drift, add skeleton for molecule.

--- src/home/molecule/molecule.js
+++ src/home/molecule/molecule.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 
 const Molecule = (props) => {
     const [active, setActive] = React.useState(0);
     return (
         <>
+            {props.items.map((v, i) => (
+                <div className={`atom-wrapper`}>
+                    <div className={`atom atom-${i}`}>
+                        <Typography>{v}</Typography>
+                    </div>
+                    <div className={`atom-${i}-bond`}/>
+                </div>
+            ))}
         </>
     )
 }
